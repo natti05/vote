640beda7e819efd67d4f2f1827f266e484f43a8c
Natalia Takahara <nana_takahara@hotmail.com>
2020-05-07 12:15:49 +0900

    added layout and post page

--- favorite.rb
+++ favorite.rb
@@ -1,7 +1,14 @@
 require 'sinatra'
 
 get '/' do
+  @greeting = 'welcome!'
   erb :index
+end
+
+post '/cast' do
+  @greeting = 'Thank you for your vote!'
+  @vote = params['vote']
+  erb :cast
 end
 
 Options = {
